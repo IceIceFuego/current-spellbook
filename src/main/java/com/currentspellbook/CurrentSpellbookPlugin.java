da32be8dbcb88736e53ba6d9ddad934974853f4f
IceIceFuego <synthsofrage@outlook.com>
2025-11-01 07:20:57 -0700

    Revert unwanted changes

--- src/main/java/com/currentspellbook/CurrentSpellbookPlugin.java
+++ src/main/java/com/currentspellbook/CurrentSpellbookPlugin.java
@@ -4,15 +4,10 @@ import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
-import net.runelite.client.config.Config;
-import net.runelite.client.config.ConfigDescriptor;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
-import net.runelite.client.plugins.PluginManager;
 import net.runelite.client.ui.overlay.OverlayManager;
-
-import java.util.Objects;
 
 @Slf4j
 @PluginDescriptor(
@@ -33,12 +28,6 @@ public class CurrentSpellbookPlugin extends Plugin
 
 	@Inject
 	private CurrentSpellbookOverlay currentSpellbookOverlay;
-
-	@Inject
-	private PluginManager pluginManager;
-
-	@Inject
-	private ConfigManager configManager;
 
 	@Override
 	protected void startUp() throws Exception
@@ -63,16 +52,4 @@ public class CurrentSpellbookPlugin extends Plugin
 		return client.getVarbitValue(SPELLBOOK_VARBIT);
 	}
 
-	void getSetupSpellbook()
-	{
-		Config config;
-		for (Plugin p : pluginManager.getPlugins())
-		{
-			if (p.getName().equals("inventory-setups"))
-			{
-				config = pluginManager.getPluginConfigProxy(p);
-				ConfigDescriptor configDescriptor = configManager.getConfigDescriptor(config);
-			}
-		}
-	}
 }
